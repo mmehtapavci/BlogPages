31d03e2d466ac50bf00f78d73730fe032b1515d5
mmehtapavci <mmmehtapavci@gmail.com>
2023-11-19 00:01:07 +0300

    Form Submit add

--- main_blog.js
+++ main_blog.js
@@ -26,8 +26,40 @@ $(document).ready(function () {
     });
 });
 
+// HTML İLE YAPILAN ÜYE GİRİŞİ
 $(function () {
-    $("#btn_giris").click(function () {
-        $("#myUye").modal('show');
+    $("#btn-giris").click(function () {
+        $("#myUye").modal();
+    });
+});
+
+// user_email user_phone user_ipassword
+// Form validation Jquery
+$(function () {
+    $("#btnSubmit").click(function () {
+
+        var email, phone, password;
+        email = $("#user_email").val();
+        phone = $("#user_phone").val();
+        password = $("#user_password").val();
+
+        // validation_email validation_phone validation_password
+        if (email == "") {
+            $("#validation_email").html("Email boş geçilemez!");
+        } else if (validateEmail(email) == false) {
+            $('#validation_email').html("E-maili Uygun Formatta Giriniz..");
+        }
+
+        if (phone == "") {
+            $("#validation_phone").html("Telefon boş geçilemez!");
+        } else if ($.isNumeric(phone) == false) {
+            $('#validation_phone').html("Sayı Girmelisiniz..");
+        }
+
+        if (password == "") {
+            $("#validation_password").html("Şifre Boş Geçilemez!");
+        } else {
+            // Add your additional password validation logic here
+        }
     });
 });
